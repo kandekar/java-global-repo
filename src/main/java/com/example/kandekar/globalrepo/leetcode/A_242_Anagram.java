e22489ed237ea36304db9687b7b9212479144d8d
SANDEEP KANDEKAR <sandeep@sandeeps-mbp.lan>
2022-06-27 22:53:47 -0400

    initial commit

---
+++ src/main/java/com/example/kandekar/globalrepo/leetcode/A_242_Anagram.java
@@ -0,0 +1,8 @@
+package com.example.kandekar.globalrepo.leetcode;
+
+public class A_242_Anagram {
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/example/kandekar/globalrepo/GlobalRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kandekar.globalrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
